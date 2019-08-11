f8a3a0c3cbbac8701a08f2ebba9a09b99cf26713
Michael Olbrich <m.olbrich@pengutronix.de>
2019-08-11 12:58:01 +0200

    image-toolchain-tgz: strip host binaries only for the tarball
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- rules/image-toolchain-tgz.make
+++ rules/image-toolchain-tgz.make
@@ -17,20 +17,18 @@ IMAGE_PACKAGES-$(PTXCONF_IMAGE_TOOLCHAIN_TGZ) += image-toolchain-tgz
 # Paths and names
 #
 IMAGE_TOOLCHAIN_TGZ_VERSION	:= $(shell ./scripts/setlocalversion ./.tarball-version)
+IMAGE_TOOLCHAIN_TGZ		:= image-toolchain-tgz-$(IMAGE_TOOLCHAIN_TGZ_VERSION)
+IMAGE_TOOLCHAIN_TGZ_DIR		:= $(BUILDDIR)/$(IMAGE_TOOLCHAIN_TGZ)
 IMAGE_TOOLCHAIN_TGZ_IMAGE	:= $(PTXDIST_WORKSPACE)/dist/oselas.toolchain-$(IMAGE_TOOLCHAIN_TGZ_VERSION)-$(subst _,-,$(PTXCONF_PLATFORM))_$(PTX_TOOLCHAIN_HOST_ARCH).tar.xz
 
 # ----------------------------------------------------------------------------
 # Image
 # ----------------------------------------------------------------------------
 
-$(IMAGE_TOOLCHAIN_TGZ_IMAGE): $(STATEDIR)/world.cleanup
+$(IMAGE_TOOLCHAIN_TGZ_IMAGE): $(STATEDIR)/world.targetinstall
 	@$(call targetinfo)
-	@mkdir -p $(dir $@)
-	@echo 'tar -C "$(PTX_AUTOBUILD_DESTDIR)$(PTXCONF_PREFIX)" \
-		--exclude=gcc-first \
-		-cJf "$(@)" \
-		"${PTXCONF_PROJECT}/$(PTXCONF_GNU_TARGET)/$(PTXCONF_PREFIX_TCID)"' \
-		| fakeroot
+	@$(call world/image/env, IMAGE_TOOLCHAIN_TGZ) \
+		ptxd_make_image_tgz
 	@$(call finish)
 
 # vim: syntax=make
---
+++ scripts/lib/ptxd_make_image_tgz.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Copyright (C) 2019 by Michael Olbrich <m.olbrich@pengutronix.de>
+#
+# For further information about the PTXdist project and license conditions
+# see the README file.
+#
+
+
+ptxd_make_image_tgz() {
+    ptxd_make_image_init || return
+
+    local prefix_cross="$(ptxd_get_ptxconf PTXCONF_PREFIX_CROSS)"
+    local src="${PTX_AUTOBUILD_DESTDIR}${prefix_cross}"
+    local dst="${pkg_dir}/$(dirname ${prefix_cross})"
+    local sysroot="${pkg_dir}${prefix_cross}"
+    local -a host_dirs=( \
+	"${sysroot}/lib" \
+	"${sysroot}/libexec" \
+	"${sysroot}/bin" \
+	"${sysroot}/${ptxconf_gnu_target}/bin" \
+    )
+
+    rm -rf "${pkg_dir}" &&
+    rm -f "${image_image}" &&
+    mkdir -p "${dst}" &&
+    cp -a "${src}" "${dst}" || return
+
+    # strip all host binaries
+    find "${host_dirs[@]}" \
+	-wholename "${PTXDIST_SYSROOT_CROSS}${prefix_cross}/lib/gcc" -prune -o \
+	-type f \( -executable -o -name "*.so*" \) -print0 \
+	| xargs -0 -n1 --verbose strip --preserve-dates
+
+    mkdir -p "$(dirname "${image_image}")" &&
+    echo "Creating $(ptxd_print_path "${image_image}") ..." &&
+    cd "${pkg_dir}/$(ptxd_get_ptxconf PTXCONF_PREFIX)" &&
+    echo "tar -cJf '${image_image}' *" | fakeroot &&
+    rm -rf "${pkg_dir}"
+}
+export -f ptxd_make_image_tgz
