9934d6f577302e34aff42cbb8faac5e6baa26a0a
Michael Olbrich <m.olbrich@pengutronix.de>
2019-09-04 12:24:03 +0200

    gencontrol.sh: update latest version from sid
    We have on explicit Python version in the dependencies. This is needed to
    build that 'sid' packages with a Python version that is not yet the
    default. Update this version to 3.8.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- debian/bin/gencontrol.sh
+++ debian/bin/gencontrol.sh
@@ -24,7 +24,7 @@ Priority: optional
 Maintainer: PTXdist Devevelopers <ptxdist@pengutronix.de>
 Homepage: https://www.pengutronix.de/software/toolchain.html
 Bugs: mailto:bugs@pengutronix.de
-Build-Depends: debhelper (>= 9), libncurses-dev, python3-dev | python3.7-dev, bison, flex
+Build-Depends: debhelper (>= 9), libncurses-dev, python3-dev | python3.8-dev, bison, flex
 EOF
 
 if [ $# -gt 0 ]; then
