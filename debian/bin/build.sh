73fbfa7ed205c36178f95b983e1c7eea175420af
Michael Olbrich <m.olbrich@pengutronix.de>
2020-05-10 16:54:54 +0200

    debian: build.sh: add version sanity check
    Creating the Debian packages will fail if the version does not match, so
    check it first.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- debian/bin/build.sh
+++ debian/bin/build.sh
@@ -14,6 +14,14 @@ else
 	exit 1
 fi
 
+cfgs=( ptxconfigs/*.ptxconfig )
+PTX_VERSION=$(sed -n -e 's/^PTXCONF_PROJECT="OSELAS.Toolchain-\(.*\)"$/\1/p' ${cfgs[0]})
+DEBIAN_VERSION="$(sed -n '1s/.*(\([^-]*\)-.*).*/\1/p' debian/changelog)"
+if [ "${PTX_VERSION}" != "${DEBIAN_VERSION}" ]; then
+	echo "release version from the debian/changelog (${DEBIAN_VERSION}) does not match '${PTX_VERSION}'"
+	exit 1
+fi
+
 if ! [[ "${VERSION}" =~ .*/sid ]]; then
 	git checkout debian/changelog
 	dch --local ${PREFIX}${VERSION}+ --distribution ${RELEASE} "Rebuild for ${RELEASE}" || exit
