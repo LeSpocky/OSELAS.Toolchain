09f97cf4a975e7828b9b7113caaa4a10ce31f4b2
Michael Olbrich <m.olbrich@pengutronix.de>
2020-08-28 10:04:12 +0200

    release 2020.08.0
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -20,7 +20,7 @@ fixup()
 
     # version
     PTXCONF_CONFIGFILE_VERSION="2020.08.0"
-    PTXCONF_PROJECT="OSELAS.Toolchain-2020.XX.0"
+    PTXCONF_PROJECT="OSELAS.Toolchain-2020.08.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
     # defaults
