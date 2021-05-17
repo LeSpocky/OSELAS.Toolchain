8ec982ec9ae8abe2c5929eb4853d84961b195430
Michael Olbrich <m.olbrich@pengutronix.de>
2021-05-17 13:08:15 +0200

    ptxdist: version bump 2020.08.0 -> 2021.05.0
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -19,7 +19,7 @@ fixup()
     local config="${1}"
 
     # version
-    PTXCONF_CONFIGFILE_VERSION="2020.08.0"
+    PTXCONF_CONFIGFILE_VERSION="2021.05.0"
     PTXCONF_PROJECT="OSELAS.Toolchain-2020.08.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
