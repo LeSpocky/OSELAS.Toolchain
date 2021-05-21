b28f6ea53508bdd8c4b973791a5eb973b83c1544
Michael Olbrich <m.olbrich@pengutronix.de>
2021-05-21 08:12:37 +0200

    don't used old icecc versions for cross compiling
    To get a usefull target environment, we use icecc-create-env to package it,
    then extract it and strip all binaries and then package it again.
    Without the extra step the environment is huge, because the binaries are
    not stripped at that point. However, if icecc-create-env compresses the
    environment then all of this takes a long time.
    
    And glibc 2.33 fails to build with old icecc versions.
    
    So just avoid the whole issue on old distros and don't create a target
    icecc environment.
    
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- scripts/lib/ptxd_make_compiler.sh
+++ scripts/lib/ptxd_make_compiler.sh
@@ -20,9 +20,11 @@ ptxd_make_setup_target_compiler() {
     compiler_prefix="$(ptxd_get_ptxconf PTXCONF_COMPILER_PREFIX)"
 
     ptxd_lib_setup_target_wrapper &&
-    PTXDIST_ICECC_CREATE_ENV_REAL="${PTXDIST_ICECC_CREATE_ENV}" \
-    PTXDIST_ICECC_CREATE_ENV="${PTXDIST_WORKSPACE}/scripts/icecc-create-env-wrapper" \
-    ptxd_lib_setup_target_icecc
+    if [ -n "${PTXDIST_ICECC}" ] && "${PTXDIST_ICECC}" --help | grep -q ICECC_ENV_COMPRESSION; then
+	PTXDIST_ICECC_CREATE_ENV_REAL="${PTXDIST_ICECC_CREATE_ENV}" \
+	PTXDIST_ICECC_CREATE_ENV="${PTXDIST_WORKSPACE}/scripts/icecc-create-env-wrapper" \
+	ptxd_lib_setup_target_icecc
+    fi
 }
 export -f ptxd_make_setup_target_compiler
 
