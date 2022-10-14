cc857954b9943fc4bbfd094e4ad3ba802e570139
Michael Olbrich <m.olbrich@pengutronix.de>
2022-10-14 12:47:48 +0200

    ptxdist: version bump 2022.09.0 -> 2022.10.0
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -19,7 +19,7 @@ fixup()
     local config="${1}"
 
     # version
-    PTXCONF_CONFIGFILE_VERSION="2022.09.0"
+    PTXCONF_CONFIGFILE_VERSION="2022.10.0"
     PTXCONF_PROJECT="OSELAS.Toolchain-2022.XX.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
