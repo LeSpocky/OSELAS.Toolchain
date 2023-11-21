753b6e987f3ebf3e2bd6485a9888ee7394aeb21d
Michael Olbrich <m.olbrich@pengutronix.de>
2023-11-21 08:04:38 +0100

    release 2022.10.2
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -20,7 +20,7 @@ fixup()
 
     # version
     PTXCONF_CONFIGFILE_VERSION="2022.10.0"
-    PTXCONF_PROJECT="OSELAS.Toolchain-2022.10.1"
+    PTXCONF_PROJECT="OSELAS.Toolchain-2022.10.2"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
     # defaults
