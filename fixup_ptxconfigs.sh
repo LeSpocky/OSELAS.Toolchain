080a70a2d523cc1d81146aff8d89248fd72faf60
Michael Olbrich <m.olbrich@pengutronix.de>
2024-10-11 09:04:58 +0200

    ptxconfig: update version to 2024.XX.0 do distinguish devel builds from last release
    Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

--- fixup_ptxconfigs.sh
+++ fixup_ptxconfigs.sh
@@ -20,7 +20,7 @@ fixup()
 
     # version
     PTXCONF_CONFIGFILE_VERSION="2024.10.0"
-    PTXCONF_PROJECT="OSELAS.Toolchain-2023.07.0"
+    PTXCONF_PROJECT="OSELAS.Toolchain-2024.XX.0"
     PTXCONF_PROJECT="${PTXCONF_PROJECT##*/}"
 
     # defaults
